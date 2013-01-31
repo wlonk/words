d115a25bb58d67fc98c92320cb8e1df77da8712c
Kit La Touche <kit.la.t@gmail.com>
2013-01-31 16:58:17 -0700

    Added ordering critera to models.

--- words/words/models.py
+++ words/words/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Language(models.Model):
     iso = models.CharField(max_length=6, primary_key=True)
 
+    class Meta:
+        ordering = ('iso',)
+
 
 class Word(models.Model):
     language = models.ForeignKey(Language)
     value = models.CharField(max_length=100)
     profane = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('language', 'value',)
